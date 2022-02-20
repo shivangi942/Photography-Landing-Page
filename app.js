15fe6d5d2a57f12eafdd5a85f4a67d8f7248172a
Shivangi Dubey <shivangi942@users.noreply.github.com>
2022-02-20 22:18:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let timerBox = document.querySelector(".timer");
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    timerBox.innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i}; 
+    return i;
+  }
+
